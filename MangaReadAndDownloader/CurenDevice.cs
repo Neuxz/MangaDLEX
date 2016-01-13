00ed31349c6c38f27b51ab49de0943db39978e34
Neuxz <Benedikt.oleksik@hotmail.de>
2016-01-13 17:49:27 +0100

    First Commit

---
+++ MangaReadAndDownloader/CurenDevice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaReadAndDownloader
+{
+    class CurenDevice
+    {
+        public string CD = "";
+        public string MaName = "";
+        public int PCC = 0;
+        public string Dmin = "";
+        public string NameU = "";
+        public bool IA = false;
+        public bool prxy = false;
+    }
+}
---
+++ MangaReadAndDownloader/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaReadAndDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CurenDevice cdir = new CurenDevice();
+            cdir.CD = Environment.CurrentDirectory;
+            cdir.MaName = Environment.MachineName;
+            cdir.PCC = Environment.ProcessorCount;
+            cdir.Dmin = Environment.UserDomainName;
+            cdir.NameU = Environment.UserName;
+            cdir.IA = Environment.UserInteractive;
+            Ping pingReq = new Ping();
+            try
+            {
+                PingReply pirpy = pingReq.Send("www.google.de");
+                if (pirpy == null)
+                { cdir.prxy = false; };
+
+                cdir.prxy = (pirpy.Status != IPStatus.Success);
+
+            }
+            catch (PingException pie)
+            {
+                cdir.prxy = true;
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.Append("CurrentDirectory: " +
+                cdir.CD);
+            sb.Append("\r\n");
+            sb.Append("Devicename: " +
+                cdir.MaName);
+            sb.Append("\r\n");
+            sb.Append("ProzessorCount: " +
+                cdir.PCC);
+            sb.Append("\r\n");
+            sb.Append("CurrentDomain: " +
+                cdir.Dmin);
+            sb.Append("\r\n");
+            sb.Append("User :" +
+                cdir.NameU);
+            sb.Append("\r\n");
+            sb.Append("This.IsAktive: " +
+                cdir.IA);
+            sb.Append("\r\n");
+            sb.Append("Proxy is: " + (cdir.prxy ? "Aktive" : "Not Aktive"));
+
+            Console.WriteLine(sb.ToString());//SET
+            Console.ReadKey();
+        }
+    }
+}
